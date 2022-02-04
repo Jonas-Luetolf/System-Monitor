a4362b638fda1130a09967c2927fb4194d43058c
jonas-luetolf <jonas.luetolf@outlook.com>
2022-02-04 12:55:21 +0100

    update Makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 run: System-Monitor.py
-	python3.10 Jakobsstab.py
+	python3.10 System-Monitor.py --loop
 compile:
 	python3.10 setup.py build
--- System-Monitor.py
+++ System-Monitor.py
@@ -25,4 +25,7 @@ def main()->None:
 
    
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as exc:
+        print(exc)
