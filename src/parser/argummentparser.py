1d54b8f23c6e380463bcf03f74335da205146ea5
jonas-luetolf <jonas.luetolf@outlook.com>
2022-02-11 09:03:27 +0100

    replace argummentparser

---
+++ src/parser/argummentparser.py
@@ -0,0 +1,47 @@
+import sys
+class ParseError(Exception):
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return "ParseError"
+
+class Flag:
+    def __init__(self,flag:str,options:int,flag_symbol="-"):
+        self.flag=flag
+        self.options=options
+        self.symbol=flag_symbol
+
+    def __str__(self):
+        return self.symbol*((len(self.flag)>1)+1)+self.flag
+
+class ArgummentParser:
+    def __init__(self,commands:list):
+        self.commands=commands
+        self.flags=[]
+
+    def add_flag(self,flag:str,options:int):
+            self.flags.append(Flag(flag,options))
+
+    def parse(self,to_parse:list):
+        x=0
+        ret_command=None
+        ret_flags={}
+        while x<len(to_parse):
+            aktuell_arg=to_parse[x]
+
+            if aktuell_arg in self.commands and ret_command==None:
+                ret_command=aktuell_arg
+                x+=1
+
+            elif aktuell_arg in (str(i) for i in self.flags):
+                for flag in self.flags:
+                    if str(flag)==aktuell_arg:
+                        ret_flags.update({str(flag):to_parse[x+1:x+2+flag.options]})
+                        x=x+2+flag.options
+                        break
+
+            else:
+                print(x)
+                raise ParseError
+        return ret_command,ret_flags
--- system-monitor.py
+++ system-monitor.py
@@ -1,27 +1,29 @@
 import src.backend as backend
 import src.frontend as frontend
 import src.diagram.diagram as diagram
-import src.parser.parser as parser
+import src.parser.argummentparser as argummentparser
 import sys
-from os.path import *
+from os.path import expanduser
 
 def main()->None: 
-    argparser=parser.ArgumentParser()
-    argparser.add_option("--loop")
-    argparser.add_option("--setconf",1)
-    argparser.parse()
+    
+    #argumment parsing
+    arg_parser=argummentparser.ArgummentParser(["loop","setconf"])
+    arg_parser.add_flag("file",1)
+    command,flags=arg_parser.parse(sys.argv[1:])
 
+    #init frontend & backend
     backend_handler=backend.Handler(f"{expanduser('~')}/.config/System-Monitor/config.yaml")
     cpu_usage_diagram=diagram.Diagram()
     Frontend=frontend.frontend(backend_handler,cpu_usage_diagram)
 
-#only python3.10 and higher can execute the code
-    match argparser[0][0]:
-        case "--loop":
+    #only python3.10 and higher can execute the code
+    match command:
+        case "loop":
             Frontend.start_loop()
 
-        case "--setconf":
-            backend_handler.set_config_by_file(argparser[0][1]) 
+        case "setconf":
+            backend_handler.set_config_by_file(flags["--file"][0]) 
 
    
 if __name__ == '__main__':
