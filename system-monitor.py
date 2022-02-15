e21ee48a40e83ba3170ff68c02aca846519b8815
Jonas-Luetolf <jonas.luetolf@outlook.com>
2022-02-15 20:22:48 +0100

    v2.0.1

--- system-monitor.py
+++ system-monitor.py
@@ -37,4 +37,8 @@ def main()->None:
             print("no command")
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    
+    except KeyboardInterrupt:
+        print("\nstop programm")
