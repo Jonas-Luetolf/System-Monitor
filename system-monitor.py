6216b16cba42c924b90e039ebed4216d1998e37d
Jonas-Luetolf <jonas.luetolf@outlook.com>
2022-02-15 20:38:43 +0100

    v2.0.1

--- system-monitor.py
+++ system-monitor.py
@@ -6,13 +6,14 @@ from sys import argv
 from os.path import expanduser
 
 def main()->None: 
-    
     #argumment parsing
     arg_parser=ArgummentParser(["loop","setconf"])
     arg_parser.add_flag("file",1)
     arg_parser.add_flag("config",1)
-    command,flags=arg_parser.parse(argv[1:])
-
+    try:
+        command,flags=arg_parser.parse(argv[1:])
+    except ParseError:
+        print("Unknown command or flag")
     #find config path
     if "--config" in flags:
         config_path=flags["--config"][0]
@@ -31,7 +32,10 @@ def main()->None:
             frontend.start_loop()
 
         case "setconf":
-            backend_handler.set_config_by_file(flags["--file"][0]) 
+            if "--file" in flags:
+                backend_handler.set_config_by_file(flags["--file"][0]) 
+            else:
+                print("flag --file required")
 
         case _:
             print("no command")
